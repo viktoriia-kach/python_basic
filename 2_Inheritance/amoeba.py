6661b1104e60ea17276433a12138bba47629a808
viktoriia-kach <tori11f92@ukr.net>
2026-03-13 13:51:51 +0200

    create class amoeba

---
+++ 2_Inheritance/amoeba.py
@@ -0,0 +1,19 @@
+class Amoeba:
+    organization = "single-cell"
+    habitation = "water"
+
+    def __init__(self, name):
+        self.name = name
+
+    @classmethod
+    def describe(cls):
+        return (f"{cls.__name__} has {cls.organization} organization"
+                f" and lives in {cls.habitation}.")
+
+    @staticmethod
+    def move(direction):
+        print(f"Moves to the {direction} with pseudopodia")
+
+    def eat(self, subject):
+        print(f"{self.name.capitalize()} grows pseudopodia"
+              f" to eat {subject}")
